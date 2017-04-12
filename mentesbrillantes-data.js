a175387d8454637ce39d3afbf9cb5318c291ce5f
atelierdechile <atelierdechile@users.noreply.github.com>
2017-04-12 17:05:05 -0300

    Update mentesbrillantes-data.js

--- mentesbrillantes-data.js
+++ mentesbrillantes-data.js
@@ -4,85 +4,57 @@ var treeData = [
     "parent": "null",
     "children": [
       {
-            "name": "Temporada 3"
+            "name": "Temporada 1"
             ,"_children": [
                {  
-                "name":   "Marketing Cloud",
+                "name":   "Mentes Brillantes 1 - Pablo Valenzuela",
                  "url": "https://www.youtube.com/v/joXU0YgKu4Q"
               }
               ,{  
-                "name":   "Customer Journey",
+                "name":   "Mentes Brillantes 1 - Cecilia Hidalgo",
                  "url": "https://www.youtube.com/v/OUWwZcVA-mM"
               }
              ,{  
-                "name":   "Social Studio",
+                "name":   "Mentes Brillantes 1 - Ricardo Baeza",
                  "url": "https://www.youtube.com/v/27P6am1sIFQ"
               },
               {  
-                "name":   "Best Email Practices",
+                "name":   "Mentes Brillantes 1 - Carlos Conca",
                  "url": "https://www.youtube.com/v/UqRGfbPsTAI"
               }
               ,
               {  
-                "name":   "Social Best Practices for Events",
+                "name":   "Mentes Brillantes 1 - Eric Goles",
                  "url": "https://www.youtube.com/v/hDjqBEy-a4M"
               }
                ,
               {  
-                "name":   "Deliverability Guide to the Inbox",
+                "name":   "Mentes Brillantes 1 - Nibaldo Inestroza",
                  "url": "https://www.youtube.com/v/NUEt3cZmSTo"
               }
                ,
               {  
-                "name":   "Achieve List Growth Results",
+                "name":   "Mentes Brillantes 1 - Juan Asenjo",
                  "url": "https://www.youtube.com/v/64ERoj3wpxw"
               },
               
-    {
-    "name":    "DonorsChoose.org Customer Success"
-     ,"url":   "https://www.youtube.com/v/v66RVWKwO2k"
-    },
+             {
+                "name":    "Mentes Brillantes 1 - María Teresa Ruiz"
+                ,"url":   "https://www.youtube.com/v/v66RVWKwO2k"
+             },
 
-    { "name":    "Stanley Black & Decker Customer Success"
-       ,"url":   "https://www.youtube.com/v/bSSpSnfjI28"
-    } , 
+             { "name":    "Mentes Brillantes 1 - Mario Luxoro"
+             ,"url":   "https://www.youtube.com/v/bSSpSnfjI28"
+             } , 
 
-    { "name":    "Live Nation Customer Success"
-       ,"url":   "https://www.youtube.com/v/rfpcOoRv7Ww"
-    },
-    {
-     "name":    "Unilever Customer Success"
-      ,"url":   "https://www.youtube.com/v/8EssPctyL0M"
-    },
-
-     {"name":    "Royal Bank of Canada Customer Success"
-      ,"url":    "https://www.youtube.com/v/lOqvGs8r0MI"
-     },
-    {
-      "name":   "Philips Customer Success"
-       ,"url":   "https://www.youtube.com/v/_UrGfHAshZk"
-    }
-
-    ,{ "name":    "Microsoft Customer Success"
-       ,"url":   "https://www.youtube.com/v/or16DB2qcXk"
-    },
-
-     {"name":    "Sony Playstation Customer Success"
-      ,"url":  "https://www.youtube.com/v/Pg08zNAqYDU"
-    },
-
-     {"name":    "Minacs Solutions Customer Success"
-       ,"url":  "https://www.youtube.com/v/V08WobkXA3Y"
-    },
-
-    { "name":    "General Electric Customer Success"
-       , "url":  "https://www.youtube.com/v/nHaBCcn-9BU"
-    },
-
-     {"name":    "News Corp Australia Customer Success"
-       ,"url":   "https://www.youtube.com/v/I3aefSZtd8w"
-    }
-
+             { "name":    "Mentes Brillantes 1 - Fernando Mönckeberg"
+             ,"url":   "https://www.youtube.com/v/rfpcOoRv7Ww"
+             },
+          
+              {
+              "name":    "Mentes Brillantes 1 - Ramón Latorre"
+              ,"url":   "https://www.youtube.com/v/8EssPctyL0M"
+              }
  
 ]
  
