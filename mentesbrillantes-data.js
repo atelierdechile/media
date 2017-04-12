5e1a67a9f3f233fe88080439072f8dde7a6021a3
atelierdechile <atelierdechile@users.noreply.github.com>
2017-04-12 16:56:41 -0300

    Update mentesbrillantes-data.js

--- mentesbrillantes-data.js
+++ mentesbrillantes-data.js
@@ -4,64 +4,9 @@ var treeData = [
     "parent": "null",
     "children": [
       {
-        "name": "Temporada 1"
-        ,"children": [
-          {
-            "name": "Salesforce Technical Archtecture",
-            "parent": "Architect",
-            "_children": [
-              {  
-                "name":   "Multi-tenant Architecture",
-                 "url": "https://www.youtube.com/v/jrKA3cJmoms"
-              }
- 
-            ]
-         },
-          {
-            "name": "Salesforce Security Model"
+            "name": "Temporada 3"
             ,"_children": [
-             {  
-                "name":   "Salesforce Data Access and Security",
-                 "url": "https://www.youtube.com/v/17dr2GMvgd8"
-              },
-
-              {  
-                "name":   "Who Sees What",
-                 "url": "https://www.youtube.com/v/X3Hg6OXhPO8"
-              },
-              {  
-                "name":   "Introduction to the Salesforce Security Model",
-                 "url": "https://www.youtube.com/v/c1ccSXlVjXk"
-              }
-
-            ]
- 
-          },
-          {
-            "name": "Sales Cloud"
-            ,"_children": [
-              {  
-                "name":   "Get Started with Sales Cloud Administration",
-                 "url": "https://www.youtube.com/v/MSp61khlP9s"
-              }
-            ]
-
-          },
-          {
-            "name": "Service Cloud"
-            ,"_children": [
-              {  
-                "name":   "Get Started with Service Cloud Administration",
-                 "url": "https://www.youtube.com/v/fJFlIqtlscI"
-              }
- 
-            ]
- 
-          },
-          {
-            "name": "Marketing Cloud"
-            ,"_children": [
-              {  
+               {  
                 "name":   "Marketing Cloud",
                  "url": "https://www.youtube.com/v/joXU0YgKu4Q"
               }
@@ -91,172 +36,60 @@ var treeData = [
               {  
                 "name":   "Achieve List Growth Results",
                  "url": "https://www.youtube.com/v/64ERoj3wpxw"
-              }
- 
-
- 
-            ]
- 
-
-          },
-          {
-            "name": "Communities"
-            ,"_children": [
-              {  
-                "name":   "Communities",
-                 "url": "https://www.youtube.com/v/iVS0p7xAVLQ"
-              }
- 
-            ]
- 
-          },
-          {
-            "name": "Single Sign-on (SSO)"
-            ,"_children": [
-              {  
-                "name":   "SSO (Single Sign-on)",
-                 "url": "https://www.youtube.com/v/-jm5_PAxtSc"
-              }
- 
-            ]
- 
-          },
-          {
-            "name": "Integration"
-            ,"_children": [
-              {  
-                "name":   "Integration",
-                 "url": "https://www.youtube.com/v/-NLUf7O4NX0"
               },
-              {  
-                "name":   "Integration Patterns",
-                 "url": "https://www.youtube.com/v/Bt970DPPLtY"
-              }
- 
-            ]
- 
-         }
+              
+    {
+    "name":    "DonorsChoose.org Customer Success"
+     ,"url":   "https://www.youtube.com/v/v66RVWKwO2k"
+    },
 
-        ]
-      },
-      {
-        "name": "Temporada 2"
-        ,"_children": [
-          {
-            "name": "Salesforce Security Model"
+    { "name":    "Stanley Black & Decker Customer Success"
+       ,"url":   "https://www.youtube.com/v/bSSpSnfjI28"
+    } , 
 
-            ,"_children": [
-             {  
-                "name":   "Salesforce Data Access and Security",
-                 "url": "https://www.youtube.com/v/17dr2GMvgd8"
-              },
+    { "name":    "Live Nation Customer Success"
+       ,"url":   "https://www.youtube.com/v/rfpcOoRv7Ww"
+    },
+    {
+     "name":    "Unilever Customer Success"
+      ,"url":   "https://www.youtube.com/v/8EssPctyL0M"
+    },
 
-              {  
-                "name":   "Who Sees What",
-                 "url": "https://www.youtube.com/v/X3Hg6OXhPO8"
-              },
-              {  
-                "name":   "Introduction to the Salesforce Security Model",
-                 "url": "https://www.youtube.com/v/c1ccSXlVjXk"
-              }
+     {"name":    "Royal Bank of Canada Customer Success"
+      ,"url":    "https://www.youtube.com/v/lOqvGs8r0MI"
+     },
+    {
+      "name":   "Philips Customer Success"
+       ,"url":   "https://www.youtube.com/v/_UrGfHAshZk"
+    }
 
-            ]
+    ,{ "name":    "Microsoft Customer Success"
+       ,"url":   "https://www.youtube.com/v/or16DB2qcXk"
+    },
 
-          },
-          {
-            "name": "Sales Cloud"
-             ,"_children": [
-              {  
-                "name":   "Get Started with Sales Cloud Administration",
-                 "url": "https://www.youtube.com/v/MSp61khlP9s"
-              }
-            ]
-          },
-          {
-            "name": "Service Cloud"
-             ,"_children": [
-              {  
-                "name":   "Get Started with Service Cloud Administration",
-                 "url": "https://www.youtube.com/v/fJFlIqtlscI"
-              }
+     {"name":    "Sony Playstation Customer Success"
+      ,"url":  "https://www.youtube.com/v/Pg08zNAqYDU"
+    },
+
+     {"name":    "Minacs Solutions Customer Success"
+       ,"url":  "https://www.youtube.com/v/V08WobkXA3Y"
+    },
+
+    { "name":    "General Electric Customer Success"
+       , "url":  "https://www.youtube.com/v/nHaBCcn-9BU"
+    },
+
+     {"name":    "News Corp Australia Customer Success"
+       ,"url":   "https://www.youtube.com/v/I3aefSZtd8w"
+    }
+
  
-            ]
-          },
-          {
-            "name": "Marketing Cloud"
-            ,"_children": [
-              {  
-                "name":   "Marketing Cloud",
-                 "url": "https://www.youtube.com/v/joXU0YgKu4Q"
-              }
-              ,{  
-                "name":   "Customer Journey",
-                 "url": "https://www.youtube.com/v/OUWwZcVA-mM"
-              }
-             ,{  
-                "name":   "Social Studio",
-                 "url": "https://www.youtube.com/v/27P6am1sIFQ"
-              }
-
-
-            ]
+]
  
-          },
-          {
-            "name": "Communities"
-            ,"_children": [
-              {  
-                "name":   "Communities",
-                 "url": "https://www.youtube.com/v/iVS0p7xAVLQ"
-              }
- 
-            ]
- 
-
-          },
-          {
-            "name": "Single Sign-on (SSO)"
-            ,"_children": [
-              {  
-                "name":   "SSO (Single Sign-on)",
-                 "url": "https://www.youtube.com/v/-jm5_PAxtSc"
-              }
- 
-            ]
- 
-          },
-          {
-            "name": "Release Management"
-            ,"_children": [
-              {  
-                "name":   "Introduction to Salesforce Release Management",
-                 "url": "https://www.youtube.com/v/afK95S3UeyU"
-              }
- 
-            ]
- 
-          },
-          {
-            "name": "Apex"
-            ,"_children": [
-              {  
-                "name":   "Apex Coding",
-                 "url": "https://www.youtube.com/v/Azb31kE31ZA"
-              }
- 
-            ]
- 
-          }
-
-
-
-        ]
-
-      },
-      
+},
     
 {
-            "name": "Temporada 3"
+            "name": "Temporada 2"
             ,"_children": [
                {  
                 "name":   "Marketing Cloud",
