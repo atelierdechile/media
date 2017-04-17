7a764cc1b40b7445256159cf67ed2a26d77c1a7f
atelierdechile <atelierdechile@users.noreply.github.com>
2017-04-17 14:50:04 -0300

    Update mentesbrillantes-data.js

--- mentesbrillantes-data.js
+++ mentesbrillantes-data.js
@@ -8,7 +8,7 @@ var treeData = [
             ,"_children": [
                {  
                 "name":   "Mentes Brillantes 1 - Pablo Valenzuela",
-                 "url": "https://www.youtube.com/watch?v=ZmzUksziaCg"
+                 "url": "https://www.youtube.com/watch?v=ZmzUksziaCg&list=PLxkqAm6-kLJh6xYS0b5ZxpcLXLsoKq74b&index=1&t=89s"
               }
              ,{  
                 "name":   "Mentes Brillantes 1 - Ricardo Baeza",
@@ -65,7 +65,7 @@ var treeData = [
               }
               ,{  
                 "name":   "Mentes Brillantes 1 - Cecilia Hidalgo",
-                 "url": "https://www.youtube.com/watch?v=BENmwqYWRVM"
+                 "url": "https://www.youtube.com/watch?v=BENmwqYWRVM&list=PLxkqAm6-kLJh6xYS0b5ZxpcLXLsoKq74b&index=2"
               }
               ,{  
                 "name":   "Customer Journey",
